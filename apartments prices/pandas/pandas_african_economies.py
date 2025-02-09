3a122c9303af5bca92e1578863faef98c216694d
georgevlasceanu <166035176+georgevlasceanu@users.noreply.github.com>
2025-02-09 19:20:11 +0200

    Add files via upload

---
+++ apartments prices/pandas/pandas_african_economies.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# Citeste din fisierul  'african_econ_crises.csv'
+
+## Citeste primele 5 randuri
+
+## Citeste 10 randuri
+
+## Citeste coloana cu tari
+
+## Printeaza tarile, o singura data
+
+### Crearea unei serii noi in functie de o conditie 
+
+### Filtrare dupa o tara (ex: Algeria)
+
+## Sortarea dupa o anumita valoare
+
+## Salveaza valorile intr-un json
